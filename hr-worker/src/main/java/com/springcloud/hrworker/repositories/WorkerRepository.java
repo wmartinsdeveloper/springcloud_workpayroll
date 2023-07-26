b1e313094b984c7eb1a57323d6e907fba32d67e4
Wellington Martins <wmartinsdeveloper@gmail.com>
2023-07-26 15:26:06 -0300

    hr-worker implementation

---
+++ hr-worker/src/main/java/com/springcloud/hrworker/repositories/WorkerRepository.java
@@ -0,0 +1,9 @@
+package com.springcloud.hrworker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.springcloud.hrworker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long>{
+
+}
