3aeb64ae4a85b0066c5c4699e0d1e8cecbc9ee2a
Wellington Martins <wmartinsdeveloper@gmail.com>
2023-07-30 21:01:37 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/springcloud/oauth/entities/Users.java
+++ hr-oauth/src/main/java/com/springcloud/oauth/entities/Users.java
@@ -1,12 +1,18 @@
 package com.springcloud.oauth.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 
-public class Users implements Serializable {
+public class Users implements UserDetails, Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -79,6 +85,43 @@ public class Users implements Serializable {
 	@Override
 	public String toString() {
 		return "users [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + "]";
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return roles.stream().map(x -> new SimpleGrantedAuthority(x.getRoleName()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
 	}
 
 	
--- hr-oauth/src/main/java/com/springcloud/oauth/service/UserService.java
+++ hr-oauth/src/main/java/com/springcloud/oauth/service/UserService.java
@@ -3,25 +3,32 @@ package com.springcloud.oauth.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.springcloud.oauth.entities.Users;
 import com.springcloud.oauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	public static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
+
 	@Autowired
 	private UserFeignClient userFeignClient;
 	
-	public Users findByEmail(String email) {
-		Users user = userFeignClient.findByEmail(email).getBody();
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Users user = userFeignClient.findByEmail(username).getBody();
 		if (user == null) {
-			throw new IllegalArgumentException("Email not found.");		
+			logger.error("Email not found: " + username);
+			throw new UsernameNotFoundException("Email not found");
 		}
-		
+		logger.info("Email found: " + username);
 		return user;
 	}
 }
